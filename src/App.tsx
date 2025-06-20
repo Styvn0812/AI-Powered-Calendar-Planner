1dcab6de0a6c1eb6a897485c20b767d863aac967
Styvn0812 <polyn0812@gmail.com>
2025-06-20 16:52:05 +0800

    pushh

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import { CalendarProvider } from './context/CalendarContext';
 import { ChatProvider } from './context/ChatContext';
 import { GoogleCalendarProvider } from './context/GoogleCalendarContext';
 import { Layout } from './components/Layout';
+import GoogleSignIn from './components/GoogleSignIn';
 
 function App() {
+  const [googleUser, setGoogleUser] = useState<any | null>(null);
+
+  if (!googleUser) {
+    return <GoogleSignIn onSignIn={setGoogleUser} />;
+  }
+
   return (
     <GoogleCalendarProvider>
       <CalendarProvider>
